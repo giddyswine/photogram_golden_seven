6d3b536dc357dc6036ef9436b61759b776eeec91
Grant Ziesemer <gzieseme@chicagobooth.edu>
2017-07-20 01:50:50 +0000

    delete photos works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,18 +4,22 @@ class PicturesController < ApplicationController
         the_id_number = params["the_id"]
         @the_source = Photo.find(the_id_number).source
         @the_caption = Photo.find(the_id_number).caption
-    render("pic_templates/show.html.erb")
+        @the_time_created = Photo.find(the_id_number).created_at
+        @the_time_updated = Photo.find(the_id_number).updated_at
+        
+        render("pic_templates/show.html.erb")
     
     end
 
     def create_row
-        @new_photo=Photo.new
-        @last_photo_id=Photo.last.id
-        @new_photo_id=@last_photo_id+1
-        @new_photo.id=@new_photo_id
-        @new_photo.source=params["the_source"]
-        @new_photo.caption=params["the_caption"]
-        @new_photo.save
+        new_photo=Photo.new
+        last_photo_id=Photo.last.id
+        new_photo_id=last_photo_id+1
+        new_photo.id=new_photo_id
+        new_photo.source=params["the_source"]
+        new_photo.caption=params["the_caption"]
+        new_photo.save
+        @number_of_pictures=Photo.count
         
         render("pic_templates/create_row.html.erb") 
     end
@@ -23,4 +27,19 @@ class PicturesController < ApplicationController
     def new_form
         render("pic_templates/new_form.html.erb")
     end
+    
+    def index
+        render("pic_templates/index.html.erb")
+    end
+    
+    def destroy_row
+        @killed_picture=Photo.find(params["toast_id"])
+        @killed_picture.delete
+        @killed_picture.save
+        @number_of_pictures=Photo.count
+        
+        render("pic_templates/delete_row.html.erb") 
+    end
+    
+    
 end
